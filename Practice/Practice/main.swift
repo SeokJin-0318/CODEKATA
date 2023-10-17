57994c6c37a2e64f26bb86f567a10f05a9c935fa
Darwin <limsj318@naver.com>
2023-10-17 21:22:51 +0900

    commit

---
+++ Practice/Practice/main.swift
@@ -0,0 +1,8 @@
+//
+//  main.swift
+//  Practice
+//
+//  Created by 석진 on 10/17/23.
+//
+
+import Foundation
---
+++ Practice/Practice/minus.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Practice
+//
+//  Created by 석진 on 10/17/23.
+//
+
+import Foundation
+
+func solution(num1: Int,num2: Int) -> Int
+{
+    return num1 - num2
+}
+
+
+let num1 = 1, num2 = 2
+
+let result = solution(num1: num1, num2: num2)
+
+print("num1이 \(num1)이고 num2가 \(num2)이므로 \(num1) - \(num2) = \(result)을 return합니다.")
+
