c1b6f74c0dae97ee5a695f74e2c3f8f269290a04
Darwin <limsj318@naver.com>
2023-10-18 16:06:31 +0900

    더하기 코드 commit

--- Practice/Practice/minus.swift
+++ Practice/Practice/minus.swift
@@ -18,4 +18,3 @@ let num1 = 1, num2 = 2
 let result = minus(num1: num1, num2: num2)
 
 print("num1이 \(num1)이고 num2가 \(num2)이므로 \(num1) - \(num2) = \(result)을 return합니다.")
-
---
+++ Practice/Practice/plus.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  Practice
+//
+//  Created by 석진 on 10/18/23.
+//
+
+import Foundation
+
+func minus(num1: Int,num2: Int) -> Int
+{
+    return num1 + num2
+}
+
+
+let num1 = 2, num2 = 3
+
+let result = minus(num1: num1, num2: num2)
+
+print("num1이 \(num1)이고 num2가 \(num2)이므로 \(num1) + \(num2) = \(result)을 return합니다.")
+
