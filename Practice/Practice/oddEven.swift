8e3a76785bcc648261b5be5fe528b0110b6ef700
Darwin <limsj318@naver.com>
2023-10-18 17:58:47 +0900

    홀짝 판별기 코드 commit

---
+++ Practice/Practice/oddEven.swift
@@ -0,0 +1,28 @@
+//
+//  oddEven.swift
+//  Practice
+//
+//  Created by 석진 on 10/18/23.
+//
+
+import Foundation
+
+
+func oddEven(num: Int) -> String
+{
+    if num % 2 == 1
+    {
+        return "Odd"
+    }
+    
+    else
+    {
+        return "Even"
+    }
+}
+
+let number = 9
+
+let result = oddEven(num: number)
+
+print(result)
