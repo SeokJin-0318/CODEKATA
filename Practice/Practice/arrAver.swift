bffef38a52fb29cb235cdd7249b5b62c46d4e231
Darwin <limsj318@naver.com>
2023-10-18 20:44:14 +0900

    배열 평균 구하기 코드 commit

---
+++ Practice/Practice/arrAver.swift
@@ -0,0 +1,47 @@
+//
+//  arrAver.swift
+//  Practice
+//
+//  Created by 석진 on 10/18/23.
+//
+
+import Foundation
+
+
+func arrAver(array: [Int]) -> Double
+{
+    var sum = 0, arrayCount = 0
+    
+    for i in array
+    {
+        sum += i
+        arrayCount += 1
+    }
+    
+    var f_sum = 0
+    
+    print("arr : [", terminator: "")
+    for i in array
+    {
+        f_sum += i
+        if f_sum == sum
+        {
+            print("\(i)", terminator: "")
+            
+        }
+        else
+        {
+            print("\(i), ", terminator: "")
+        }
+    }
+    print("]")
+
+    return Double(sum) / Double(arrayCount)
+}
+
+
+let arr = [1, 2, 3, 4]
+
+let result = arrAver(array: arr)
+
+print("return : \(result)")
