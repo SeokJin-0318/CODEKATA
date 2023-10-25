604710a402eea99db1feb72d4a47e48c194bf76f
Darwin <limsj318@naver.com>
2023-10-25 17:28:41 +0900

    숫자 거꾸로 뒤집기 코드 commit

---
+++ Practice/Practice/reverse.swift
@@ -0,0 +1,28 @@
+//
+//  reverse.swift
+//  Practice
+//
+//  Created by 석진 on 10/20/23.
+//
+
+import Foundation
+
+
+func reverse(num: Int64) -> [Int]
+{
+    var result: [Int] = []
+    
+    for i in String(num).reversed()
+    {
+        guard let number = Int(String(i)) else{ break }
+        
+        result.append(number)
+    }
+    return result
+}
+
+let number: Int64 = 12345
+
+let result = reverse(num: number)
+
+print(result)
