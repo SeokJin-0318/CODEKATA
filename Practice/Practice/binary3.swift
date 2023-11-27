91e5a3832bb34f03a30178ad8395da7ccf55c400
Darwin <limsj318@naver.com>
2023-11-27 09:52:07 +0900

    3진법 변환/뒤집기 코드 commit

---
+++ Practice/Practice/binary3.swift
@@ -0,0 +1,41 @@
+//
+//  3진법 변환 / 뒤집기
+//  Practice
+//
+//  Created by 석진 on 11/26/23.
+//
+
+import Foundation
+
+
+func reverseBinary(number: Int) -> Int
+{
+    var quotient = number, result = 0
+    var arr: [Int] = []
+    while true
+    {
+        arr.append(quotient % 3)
+        
+        quotient /= 3
+        
+        if Int(quotient / 3) == 0
+        {
+            arr.append(1)
+            break
+        }
+    }
+    print("n : ", number)
+    print("3진법 변환 : ", arr)
+    arr = arr.reversed()
+    
+    for i in 0 ..< arr.count
+    {
+        result += arr[i] * Int(pow(3.0, Double(i)))
+    }
+    
+    print("앞뒤 반전 : ", arr)
+    print("10진법 변환 : ", result)
+    return result
+}
+
+reverseBinary(number: 45)
