f9c5167374721899d3fce6c649ac15084158e558
Darwin <limsj318@naver.com>
2023-12-14 05:30:50 +0900

    코드 수정 후 commit
    for 루프 코드 수정

--- Practice/Practice/compare_otherNumber.swift
+++ Practice/Practice/compare_otherNumber.swift
@@ -14,33 +14,26 @@ func compareString(string1: String, string2: String) -> Int
     guard let compareNumber = Int(string2) else { return 0 }
     var sumNumber = 0, exponent = Double(string2.count) - 1    // 비교해줄 숫자, 제곱 지수
     
-    let string1Arr = string1.map{(char) -> Int in return Int(String(char))!}
+    let string1_Arr = string1.map{(char) -> Int in return Int(String(char))!}        // 배열 생성
     
-    for i in 0 ..< string1Arr.count
+    for i in 0 ..< string1_Arr.count - string2.count    // 배열의 마지막 index에서 string2의 자릿수를 뺀 위치까지 루프
     {
-        for j in 0 ..< string2.count
+        for j in 0 ..< string2.count        // 비교하는 숫자의 자릿수를 정해줌
         {
-            sumNumber += Int(string1Arr[i + j]) * Int(pow(10.0, exponent))
+            sumNumber += Int(string1_Arr[i + j]) * Int(pow(10.0, exponent))
             // 제곱 지수가 세자리면 string2.count가 2 이기 때문에 2부터 시작해서 백의 자리부터 더해준다
             exponent -= 1.0     // 지수 내리기
         }
         
-        if i == string1Arr.count - string2.count
-        {
-            break
-        }
-        
-        else if  compareNumber >= sumNumber
+        if compareNumber >= sumNumber
         {
             result += 1
         }
-        
-        exponent = Double(string2.count) - 1
-        sumNumber = 0
+
+        exponent = Double(string2.count) - 1        // 지수 초기화
+        sumNumber = 0                               // 비교해줄 숫자 초기화
     }
-    
     return result
 }
 
-
-print(compareString(string1: "500220839878", string2: "7"))
+print(compareString(string1: "3141592", string2: "271"))
